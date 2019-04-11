b70f9ae9da2aa7561398a4dc65b086233470657b
Tomáš Ježek <tomas.jezek1@seznam.cz>
2019-04-11 17:43:36 +0200

    App icon

--- app/src/main/java/cz/muni/moviewishlist/MainActivity.kt
+++ app/src/main/java/cz/muni/moviewishlist/MainActivity.kt
@@ -1,12 +1,28 @@
 package cz.muni.moviewishlist
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Animation */
+        ic_logo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.splash_in)) // 300 ms
+        Handler().postDelayed({
+            ic_logo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.splash_out))
+            Handler().postDelayed({
+                ic_logo.visibility = View.GONE
+                startActivity(Intent(this, DashboardActivity::class.java))
+                finish()
+            }, 500)
+        }, 1500)
     }
 }
